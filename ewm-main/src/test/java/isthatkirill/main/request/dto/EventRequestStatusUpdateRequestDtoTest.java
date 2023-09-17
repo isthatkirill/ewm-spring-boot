bcd0e718ad6e6e195e3e82cb6465ef548727caaf
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2023-09-17 18:18:07 +0300

    test: JsonTests added for EventRequestDtos added

---
+++ ewm-main/src/test/java/isthatkirill/main/request/dto/EventRequestStatusUpdateRequestDtoTest.java
@@ -0,0 +1,51 @@
+package isthatkirill.main.request.dto;
+
+import isthatkirill.main.request.model.RequestProcessedState;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Kirill Emelyanov
+ */
+
+@JsonTest
+class EventRequestStatusUpdateRequestDtoTest {
+
+    @Autowired
+    private JacksonTester<EventRequestStatusUpdateRequestDto> json;
+
+    private EventRequestStatusUpdateRequestDto request = EventRequestStatusUpdateRequestDto.builder()
+            .requestIds(List.of(1L, 2L, 3L))
+            .status(RequestProcessedState.CONFIRMED)
+            .build();
+
+    @Test
+    @SneakyThrows
+    void eventRequestStatusUpdateRequestDtoTest() {
+        JsonContent<EventRequestStatusUpdateRequestDto> result = json.write(request);
+
+        assertThat(result)
+                .hasJsonPathStringValue("$.status", request.getStatus().name())
+                .hasJsonPathArrayValue("$.requestIds", request.getRequestIds());
+    }
+
+    @Test
+    @SneakyThrows
+    void eventRequestStatusUpdateRequestDtoWithNullFieldsTest() {
+        request.setRequestIds(null);
+        JsonContent<EventRequestStatusUpdateRequestDto> result = json.write(request);
+
+        assertThat(result)
+                .hasJsonPathStringValue("$.status", request.getStatus().name())
+                .hasEmptyJsonPathValue("$.requestIds");
+    }
+
+}
---
+++ ewm-main/src/test/java/isthatkirill/main/request/dto/ParticipationRequestDtoTest.java
@@ -0,0 +1,60 @@
+package isthatkirill.main.request.dto;
+
+import isthatkirill.main.request.model.RequestState;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Kirill Emelyanov
+ */
+
+@JsonTest
+class ParticipationRequestDtoTest {
+
+    @Autowired
+    private JacksonTester<ParticipationRequestDto> json;
+
+    private ParticipationRequestDto participationRequest = ParticipationRequestDto.builder()
+            .id(1L)
+            .requester(1L)
+            .event(1L)
+            .created(LocalDateTime.now())
+            .status(RequestState.PENDING)
+            .build();
+
+    @Test
+    @SneakyThrows
+    void participationRequestDtoTest() {
+        JsonContent<ParticipationRequestDto> result = json.write(participationRequest);
+
+        assertThat(result)
+                .hasJsonPathNumberValue("$.id", participationRequest.getId())
+                .hasJsonPathNumberValue("$.requester", participationRequest.getRequester())
+                .hasJsonPathNumberValue("$.event", participationRequest.getEvent())
+                .hasJsonPathStringValue("$.status", participationRequest.getStatus().name())
+                .hasJsonPathStringValue("$.created", participationRequest.getCreated());
+    }
+
+    @Test
+    @SneakyThrows
+    void participationRequestDtoWithNullFieldsTest() {
+        participationRequest.setCreated(null);
+        JsonContent<ParticipationRequestDto> result = json.write(participationRequest);
+
+        assertThat(result)
+                .hasJsonPathNumberValue("$.id", participationRequest.getId())
+                .hasJsonPathNumberValue("$.requester", participationRequest.getRequester())
+                .hasJsonPathNumberValue("$.event", participationRequest.getEvent())
+                .hasJsonPathStringValue("$.status", participationRequest.getStatus().name())
+                .hasEmptyJsonPathValue("$.created");
+    }
+
+}
